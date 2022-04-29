1b549ba6ae030928e5c43217c67e483c2f33f3b7
FenAbdel <abdeljalil.fenniri@gmail.com>
2022-04-29 17:07:38 +0000

    some upgrades

--- assistant.py
+++ assistant.py
@@ -3,10 +3,18 @@ import speech_recognition as sr
 import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+def print_talk(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
 with sr.Microphone() as source :
-    print("say what you want to search for ....")
-    voice = listener.listen(source)
-    command= listener.recognize_google(voice)
+    print_talk("say what you want to search for ....")
+    command= listener.recognize_google(listener.listen(source))
 
 # Go to google and search
 ggl.search(command)
