85d0cc6f5b8bc9017b8925c982ddaea7f9a862d6
FenAbdel <abdeljalil.fenniri@gmail.com>
2022-04-29 17:10:28 +0000

    some upgrades

--- assistant.py
+++ assistant.py
@@ -5,15 +5,13 @@ import pyttsx3
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[0].id)
+engine.setProperty('voice',voices[0].id)    
 
-def print_talk(text):
+with sr.Microphone() as source :
+    text = "say what you want to search for ...."
     print(text)
     engine.say(text)
     engine.runAndWait()
-
-with sr.Microphone() as source :
-    print_talk("say what you want to search for ....")
     command= listener.recognize_google(listener.listen(source))
 
 # Go to google and search
